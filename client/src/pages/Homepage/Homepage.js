6872dc37cd9133e0f623b94a7edd2809707196e8
Darinic <97019341+Darinic@users.noreply.github.com>
2022-12-07 14:56:17 +0200

    eslint

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ThinkingMan from "../../assets/img/thinking.png";
